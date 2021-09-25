887521a430d26fe7bd8a2314f3ae7e774dcce59c
Tirumala_Sai_Teja <80871045+TirumalaSaiTeja@users.noreply.github.com>
2021-09-25 07:22:31 +0530

    Create SqureRoot.py

---
+++ SqureRoot.py
@@ -0,0 +1,2 @@
+'''we can find square root of a number in two ways one is using general for loop and its time complexity is O(n) and 
+we can use two pointers concept here whose time complexity is O(logn base2)'''
